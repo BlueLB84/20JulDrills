fcf55b8982b5fbd97dae1d34489c4290ce893204
L Barbee <bluelb84@gmail.com>
2017-07-24 07:12:10 -0400

    final solution for click and enter to work

--- event_listener_drills/cat_carousel/index.js
+++ event_listener_drills/cat_carousel/index.js
@@ -1,11 +1,11 @@
-// target img with class thumbnails
-// add event listener on click / keydown 'enter'
-// 
-$(function(){
-    $('.thumbnails img').click(function(event){
-        let sourceAttr = $(event.currentTarget).attr('src');
-        let sourceAlt = $(event.currentTarget).attr('alt');
-        $('.hero img').attr('src', sourceAttr);
-        $('.hero img').attr('alt', sourceAlt);
-    });
-});
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+     const imgSrc = $(event.currentTarget).find('img').attr('src');  
+     const imgAlt = $(event.currentTarget).find('img').attr('alt');
+    
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    
+  });
+}
+
+$(handleThumbnailClicks());
